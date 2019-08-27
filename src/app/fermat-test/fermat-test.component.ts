ee1263d058bb2e37ed752509f17447cad63fadec
Jannik231002 <wolfgang.bopp@freenet.de>
2019-08-27 18:27:23 +0200

    2 files modified

--- src/app/fermat-test/fermat-test.component.ts
+++ src/app/fermat-test/fermat-test.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fermat-test.component.css']
 })
 export class FermatTestComponent implements OnInit {
-Kandidat: number = 13;
-Testanzahl: number = 5;
-Ergebnis = "noch nicht berechnet"
-constructor() { }
+  Kandidat: number = 13;
+  Testanzahl: number = 5;
+  Ergebnis = "noch nicht berechnet"
+  constructor() { }
+
+  fermattest() {
+    this.Ergebnis = "Starte Fermat-Test der Zahl"
+      + this.Kandidat + "...";
+    let zeuge = 2;
+    // Berechne zeuge^(zu testende Zahl-1) modulo zu testende Zahl
+    let zwischenergebnis = 1
+    for (let i = 0; i < this.Kandidat - 1; i++) {
+      zwischenergebnis = zwischenergebnis * zeuge;
+    }
+    zwischenergebnis = zwischenergebnis % this.Kandidat;
+    if (zwischenergebnis === 1) {
+      this.Ergebnis += "Zeuge " + zeuge + "OK...";
+    }
+    else {
+      this.Ergebnis += "Zeuge" + zeuge + "NICHT OK! ";
+    }
+  }
 
   ngOnInit() {
   }
